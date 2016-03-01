77d173ab7ceed63dc62e592ceb1beec883aa233e
milo zhao <milo.zhao@qq.com>
2016-03-01 17:20:40 +0800

    add lvs-rs.sh

---
+++ lvs-rs.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+VIP=172.16.0.10
+
+. /etc/rc.d/init.d/functions
+
+case "$1" in
+start)
+    /usr/sbin/ifconfig lo:0 $VIP broadcast $VIP netmask 255.255.255.255 up
+    /usr/sbin/route add -host $VIP dev lo:0
+    echo "1" > /proc/sys/net/ipv4/conf/lo/arp_ignore
+    echo "2" > /proc/sys/net/ipv4/conf/lo/arp_announce
+    echo "1" > /proc/sys/net/ipv4/conf/all/arp_ignore
+    echo "2" > /proc/sys/net/ipv4/conf/all/arp_announce
+    echo "RealServer Start OK"
+    ;;
+
+stop)
+    /usr/sbin/ifconfig lo:0 down
+    #/usr/sbin/route del $VIP > /dev/null 2>&1
+    echo "0" > /proc/sys/net/ipv4/conf/lo/arp_ignore
+    echo "0" > /proc/sys/net/ipv4/conf/lo/arp_announce
+    echo "0" > /proc/sys/net/ipv4/conf/all/arp_ignore
+    echo "0" > /proc/sys/net/ipv4/conf/all/arp_announce
+    echo "RealServer Stoped"
+    ;;
+
+*)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+esac
+exit 0
